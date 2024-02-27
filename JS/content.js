70135be04b6c08ff25131879b1f749128d0aa996
byeco <borayigitelik@gmail.com>
2024-02-27 20:25:00 +0300

    Bu hata yüzünden sitedeki yazılar gözükmüyor

--- JS/content.js
+++ JS/content.js
@@ -1,10 +1,2 @@
 // content.js
 
-// Sayfa yüklendiğinde başlıkları değiştir
-document.addEventListener("DOMContentLoaded", function () {
-    const titles = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
-
-    titles.forEach(function (title) {
-        title.textContent = "Optimized Title";
-    });
-});
